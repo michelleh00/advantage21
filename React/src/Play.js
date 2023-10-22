9034cb34f7076cf0145040c67b952d96c1be8be1
Sam Edwards <sjackedwards@gmail.com>
2023-10-21 22:12:01 -0400

    Changes to be committed: 	modified:   React/public/index.html 	modified:   React/src/Play.js 	modified:   React/src/Rules.js

---
+++ React/src/Play.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Play() {
+    return <h1>Blackjack Simulator</h1>;
+}
+
+export default Play;
---
+++ React/src/Rules.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Rules() {
+    return <h1>Rules Page</h1>;
+}
+
+export default Rules;
