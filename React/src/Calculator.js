c9694dab2bf3dd38a3e15195a6cbe0aaffbbb597
Sam Edwards <sjackedwards@gmail.com>
2023-10-22 20:52:37 -0400

    Changes to be committed: 	modified:   React/src/Calculator.css 	modified:   React/src/Calculator.js
    Added player hand logic.

--- React/src/Calculator.js
+++ React/src/Calculator.js
@@ -16,10 +16,13 @@ function Calculator() {
     }
   };
 
+  const resetHands = () => {
+    setPlayerHand([]);
+    setDealerCard(null);
+  };
 
   return (
     <div>
-      {/* Navbar */}
       <nav className="calculator-nav">
         <ul>
           <li><Link to="/">Back to Home</Link></li>
@@ -27,9 +30,9 @@ function Calculator() {
       </nav>
 
       <div className="Calculator">
+
         <h1>Optimal Play Calculator</h1>
 
-        {/* Card Selection */}
         <div className="card-sections">
           <section className="dealer-card-section">
             <h2>Dealer's Card</h2>
@@ -43,28 +46,27 @@ function Calculator() {
           </section>
 
           <section className="player-card-section">
-            <h2>Player's Cards</h2>
+            <h2>Select Player's Cards</h2>
             <div className="card-selection">
               {['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'].map(card => (
-                <button key={card} onClick={() => handleCardClick(card)}>
+                <button key={card} onClick={() => handleCardClick(card, 'player')}>
                   {card}
                 </button>
               ))}
             </div>
+            <button onClick={resetHands}>Reset</button> {/* Place the button here */}
           </section>
+
         </div>
 
-        {/* Middle Display Section */}
         <div className="middle-display">
+
           <section>
             <div className="dealer-display">
               {dealerCard && <img src={`/resources/${dealerCard}.png`} alt={`${dealerCard} card`} className="card-image" />}
               <div className="dealer-text">Dealer</div>
             </div>
           </section>
-
-
-
 
           <section>
             <h2>Best Play</h2>
@@ -73,9 +75,26 @@ function Calculator() {
 
           <section>
             <h2>Player's Hand</h2>
-            <div className="hand-display"></div>
-          </section>
+            <div className="player-hand-container">
+              {playerHand.map((card, index) => {
+                const totalCards = playerHand.length;
+                const middleIndex = (totalCards - 1) / 2; // This gets the center card for odd numbers of cards
+                const offset = index - middleIndex;
+                const rotationDegree = offset * 10; // This determines how much each card rotates. Adjust as needed.
 
+                return (
+                  <img
+                    src={`/resources/${card}.png`}
+                    alt={`${card} card`}
+                    className="card-in-hand"
+                    style={{
+                      transform: `translateX(${index * 30 - (totalCards * 15)}px) rotate(${rotationDegree}deg)`
+                    }}
+                  />
+                );
+              })}
+            </div>
+          </section>
 
         </div>
       </div>
