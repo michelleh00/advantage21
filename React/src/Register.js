3bb57add10581a6bd3ccf8cee8ad9ce6fa911ec3
mariamconde <condemriam@gmail.com>
2023-10-25 12:48:51 -0400

    Implemented register functionality and css

--- React/src/Register.js
+++ React/src/Register.js
@@ -1,7 +1,83 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "./Auth";
+import "./App.css";
+import "./Register.css";
 
 function Register() {
-    return <h1>Register Page</h1>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const { register } = useAuth();
+  const navigate = useNavigate();
+
+  const handleRegister = () => {
+    if (password === confirmPassword) {
+      // Passwords match, proceed with registration
+      register(username, email, password);
+      navigate("/login");
+    } else {
+      setError("Passwords do not match.");
+    }
+  };
+
+  return (
+      <div>
+        <nav className="register-nav">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calculator">Play Calculator</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+    <div className="register-container">
+        <h2>Sign Up</h2>
+        <div className="form-field">
+          <label>Username</label>
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label>Email</label>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="form-field">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <button className="register-btn" onClick={handleRegister}>Sign Up</button>
+        <div>{error && <p className="error">{error}</p>}</div>
+        <div>
+          Already have an account? <Link to="/login">Login here!</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
