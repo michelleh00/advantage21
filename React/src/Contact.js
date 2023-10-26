44b87f86cf84255b7245869057b2de9e0aaae1fe
mariamconde <condemriam@gmail.com>
2023-10-25 23:15:53 -0400

    contact page and styling

--- React/src/Contact.js
+++ React/src/Contact.js
@@ -1,31 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useAuth } from './Auth';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "./Auth";
+import "./Contact.css";
 
 function Contact() {
-  const { isAuthenticated, logout } = useAuth();
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link to="/calculator">Play Calculator</Link></li>
-          <li><Link to="/play">Simulator</Link></li>
-          <li><Link to="/rules">Rules</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          {isAuthenticated ? (
-                        <><li><Link to="/account">Account</Link></li>
-                        <li onClick={logout}>Logout</li></>) 
-                        :
-                        (<><li><Link to="/login">Login</Link></li>
-                        <li><Link to="/register">Register</Link></li></>
+    const { isAuthenticated, logout } = useAuth();
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/calculator">Play Calculator</Link>
+                    </li>
+                    <li>
+                        <Link to="/play">Simulator</Link>
+                    </li>
+                    <li>
+                        <Link to="/rules">Rules</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    {isAuthenticated ? (
+                        <>
+                            <li>
+                                <Link to="/account">Account</Link>
+                            </li>
+                            <li onClick={logout}>Logout</li>
+                        </>
+                    ) : (
+                        <>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/register">Register</Link>
+                            </li>
+                        </>
                     )}
-        </ul>
-      </nav>
+                </ul>
+            </nav>
 
-      <h1>Contact</h1>
-      <h2>contact us!</h2>
-    </div>
-  );
+            <h1>Contact</h1>
+            <div className="contact-sections">
+                <div className="contact-section">
+                    <h2>Commercial Interests:</h2>
+                    <div>
+                        <p>
+                            Phone: 1-800-564-9885
+                            <br />
+                            M-Sun 8am - 4pm EST
+                        </p>
+                        <p>Email: Marketing@Advantage21.com</p>
+                    </div>
+                </div>
+                <div className="contact-section">
+                    <h2>All Other Questions:</h2>
+                    <div>
+                        <p>
+                            Phone: 1-704-248-9114
+                            <br />
+                            M-Sun 8am - 4pm EST
+                        </p>
+                        <p>Email: Help@Advantage21.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Contact;
