a28f2b7471688fea955348c987e26c4d146e7516
Dylan Potts <dpotts10@uncc.edu>
2023-10-25 21:37:11 -0400

    Contact Header

--- React/src/Contact.js
+++ React/src/Contact.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from './Auth';
 
 function Contact() {
-    return <h1>Contact Us</h1>;
+  const { isAuthenticated, logout } = useAuth();
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/calculator">Play Calculator</Link></li>
+          <li><Link to="/play">Simulator</Link></li>
+          <li><Link to="/rules">Rules</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          {isAuthenticated ? (
+                        <><li><Link to="/account">Account</Link></li>
+                        <li onClick={logout}>Logout</li></>) 
+                        :
+                        (<><li><Link to="/login">Login</Link></li>
+                        <li><Link to="/register">Register</Link></li></>
+                    )}
+        </ul>
+      </nav>
+
+      <h1>Contact</h1>
+      <h2>contact us!</h2>
+    </div>
+  );
 }
 
 export default Contact;
