74db6f474704a970c7ea323a53ef3cea02f14dfd
Sam Edwards <sjackedwards@gmail.com>
2023-10-26 09:05:05 -0400

    Play simulator initial logic and CSS

--- React/src/Play.js
+++ React/src/Play.js
@@ -1,7 +1,133 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Play.css';
+
+
+// TODO:
+// Nav bar
+// Implement split and (double down, maybe?) functionality
+// Real time feedback on plays, can take this from Algorith.js
+// Stylization changes
+// Port tracking to account database, when me make that.
+// Card variations other than clubs
+// Hole card differentation
+// Surrender functionality
+
+// Need to commment everything all functions at least
+
+const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 function Play() {
-    return <h1>Blackjack Simulator</h1>;
-}
+    const [playerHand, setPlayerHand] = useState([]);
+    const [dealerHand, setDealerHand] = useState([]);
+    const [gameState, setGameStatus] = useState("Deal Cards to Start");
+
+    const calculateHandValue = (hand) => {
+        let value = 0;
+        let aces = 0;
+        for (let card of hand) {
+            if (['J', 'Q', 'K'].includes(card)) {
+                value += 10;
+            } else if (card === 'A') {
+                value += 11;
+                aces += 1;
+            } else {
+                value += parseInt(card);
+            }
+        }
+
+        while (value > 21 && aces) {
+            value -= 10;
+            aces -= 1;
+        }
+
+        return value;
+    };
+
+    const dealCards = () => {
+        const playerCards = [cardValues[Math.floor(Math.random() * 13)], cardValues[Math.floor(Math.random() * 13)]];
+        const dealerCards = [cardValues[Math.floor(Math.random() * 13)], cardValues[Math.floor(Math.random() * 13)]];
+        setPlayerHand(playerCards);
+        setDealerHand(dealerCards);
+        setGameStatus("Player's Turn");
+    };
+
+    const playerHit = () => {
+        const newCard = cardValues[Math.floor(Math.random() * 13)];
+        const newHand = [...playerHand, newCard];
+        setPlayerHand(newHand);
+
+        if (calculateHandValue(newHand) > 21) {
+            setGameStatus("Player Busted! Dealer Wins!");
+        }
+    };
+
+    const playerStand = () => {
+        setGameStatus("Dealer's Turn");
+        dealerTurn();
+    };
+
+    const dealerTurn = () => {
+        let dealerPoints = calculateHandValue(dealerHand);
+        while (dealerPoints < 17) {
+            const newCard = cardValues[Math.floor(Math.random() * 13)];
+            const newHand = [...dealerHand, newCard];
+            setDealerHand(newHand);
+            dealerPoints = calculateHandValue(newHand);
+        }
+
+        if (dealerPoints > 21) {
+            setGameStatus("Dealer Busted! Player Wins!");
+        } else if (dealerPoints === calculateHandValue(playerHand)) {
+            setGameStatus("Push!");
+        } else if (dealerPoints >= calculateHandValue(playerHand)) {
+            setGameStatus("Dealer Wins!");
+        }
+        
+        else {
+            setGameStatus("Player Wins!");
+        }
+    };
+
+    const resetGame = () => {
+        setPlayerHand([]);
+        setDealerHand([]);
+        setGameStatus("Deal Cards to Start");
+    };
+    return (
+        <div className="Play">
+            <h1>Blackjack Simulator</h1>
+            <h2>{gameState}</h2>
+    
+            <div className="play-card-sections">
+                <section className="play-dealer-card-section">
+                    <h2>Dealer's Hand</h2>
+                    {dealerHand.map((card, index) => (
+                        <img src={`/resources/${card}.png`} alt={`${card} card`} className="play-card-image" key={index} />
+                    ))}
+                </section>
+    
+                <div className="play-middle-display">
+                    {gameState === "Deal Cards to Start" ? (
+                        <button className= "play-button" onClick={dealCards}>Deal Cards</button>
+                    ) : gameState === "Player's Turn" ? (
+                        <>
+                            <button className='play-button' onClick={playerHit}>Hit</button>
+                            <button className='play-button' onClick={playerStand}>Stand</button>
+                        </>
+                    ) : (
+                        <button className='play-reset-button' onClick={resetGame}>Reset Game</button>
+                    )}
+                </div>
+    
+                <section className="play-player-card-section">
+                    <h2>Player Hand</h2>
+                    {playerHand.map((card, index) => (
+                        <img src={`/resources/${card}.png`} className="play-card-image" key={index} />
+                    ))}
+                </section>
+            </div>
+        </div>
+    );
+                    }
 
 export default Play;
