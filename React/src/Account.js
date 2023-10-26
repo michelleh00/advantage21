f4768428d37babf383c9598e217798e5ed7e3558
michelleh00 <michelle.hamilton90@yahoo.com>
2023-10-25 21:16:36 -0400

    profile/account

--- React/src/Account.js
+++ React/src/Account.js
@@ -6,14 +6,6 @@ function Account() {
 
     return (
         <div>
-          <nav>
-            <ul>
-            <li><Link to="/calculator">Play Calculator</Link></li>
-            <li><Link to="/play">Simulator</Link></li>
-            <li><Link to="/rules">Rules</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
             <h1>Account Page</h1>
             <p>Username: {userDetails.username}</p>
             <p>Password: {userDetails.password}</p>
