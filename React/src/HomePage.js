6ae042685535d04555fef5e1a734d26ef2f2475e
Dylan Potts <dpotts10@uncc.edu>
2023-10-26 20:18:13 -0400

    Homepage Buttons

--- React/src/HomePage.js
+++ React/src/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from './Auth';
+import './HomePage.css';
+import './App.css';
 
 function HomePage() {
   const { isAuthenticated, logout } = useAuth();
@@ -13,18 +15,33 @@ function HomePage() {
           <li><Link to="/rules">Rules</Link></li>
           <li><Link to="/contact">Contact</Link></li>
           {isAuthenticated ? (
-                        <><li><Link to="/account">Account</Link></li>
-                        <li onClick={logout}>Logout</li></>) 
-                        :
-                        (<><li><Link to="/login">Login</Link></li>
-                        <li><Link to="/register">Register</Link></li></>
-                    )}
+            <>
+              <li><Link to="/account">Account</Link></li>
+              <li onClick={logout}>Logout</li>
+            </>
+          ) : (
+            <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            </>
+          )}
         </ul>
       </nav>
+      
+      <h1 className="advantage-heading">Advantage 21</h1>
 
-      <h1>Advantage 21!</h1>
-      <h2>Lets make this link to optimal play/trainer</h2>
-    </div>
+<div className="CalcChip">
+      <Link to="/calculator">
+        <img src="/resources/blackchip.png" alt="black chip"/>
+      </Link>
+      </div>
+<div className="PlayChip">
+      <Link to="/play">
+        <img src="/resources/redchip.png" alt="red chip"/>
+      </Link>
+      </div>
+      </div>
+      
   );
 }
 
