a2eb5c274fbe48289200acdbb55b6e30f0aa16e9
Dylan Potts <dpotts10@uncc.edu>
2023-11-15 22:58:33 -0500

    Timer changes

--- React/src/Account.js
+++ React/src/Account.js
@@ -1,97 +1,109 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from './Auth';
 import Switch from "./Switch";
 import "./Account.css";
 
-// The user's profile is displayed here. We check first if user is logged in
-// and display the appropriate nav options accordingly.
-// Switches are used to apply app variations.
-
 function Account() {
-    const navigate = useNavigate();
-    const { isAuthenticated, logout } = useAuth();
-    const { userDetails } = useAuth();
+  const navigate = useNavigate();
+  const { isAuthenticated, logout } = useAuth();
+  const { userDetails } = useAuth();
 
-    const [darkMode, setDarkMode] = useState(false);
-    const [soft17, setSoft17] = useState(false);
-    const [surrender, setSurrender] = useState(false);
-    const [handTimer, setHandTimer] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+  const [soft17, setSoft17] = useState(false);
+  const [surrender, setSurrender] = useState(false);
+  const [handTimer, setHandTimer] = useState(false);
+  const [timerDuration, setTimerDuration] = useState(10); // Initial timer duration
 
-    const handleLogout = () => {
-        logout();
-        // Redirect to the login page after logout
-        navigate('/login');
-    };
-    
-    // Apply appropriate css depending on whether or not dark mode toggle is engaged
-    useEffect(() => {
-        if (darkMode) {
-            document.body.classList.add('dark-mode');
-        } else {
-            document.body.classList.remove('dark-mode');
-        }
-    }, [darkMode]);
+  const handleLogout = () => {
+    logout();
+    // Redirect to the login page after logout
+    navigate('/login');
+  };
 
+  // Apply appropriate css depending on whether or not dark mode toggle is engaged
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [darkMode]);
 
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li><Link to="/calculator">Play Calculator</Link></li>
-                    <li><Link to="/play">Simulator</Link></li>
-                    <li><Link to="/rules">Rules</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    {isAuthenticated ? (
-                        <><li><Link to="/account">Account</Link></li>
-                            <li onClick={handleLogout}>Logout</li></>)
-                        :
-                        (<><li><Link to="/login">Login</Link></li>
-                            <li><Link to="/register">Register</Link></li></>)
-                    }
-                </ul>
-            </nav>
-            <h1>Welcome, {userDetails.username}!</h1>
+  const handleTimerChange = (duration) => {
+    setTimerDuration(duration);
+    // You can add additional logic here if needed, e.g., update server settings
+  };
 
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/calculator">Play Calculator</Link></li>
+          <li><Link to="/play">Simulator</Link></li>
+          <li><Link to="/rules">Rules</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          {isAuthenticated ? (
+            <>
+              <li><Link to="/account">Account</Link></li>
+              <li onClick={handleLogout}>Logout</li>
+            </>
+          ) : (
+            <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            </>
+          )}
+        </ul>
+      </nav>
+      <h1>Welcome, {userDetails.username}!</h1>
 
-            <div className="acct-image">
-                <img src="/resources/profile.png" alt="profile icon" />
-            </div>
+      <div className="acct-image">
+        <img src="/resources/profile.png" alt="profile icon" />
+      </div>
 
-
-            <div className={`acct-container ${darkMode ? 'dark-mode' : ''}`}>
-                <div className="left-options">
-                    Dark Mode:
-                    <br></br>
-                    Hit on Soft 17:
-                    <br></br>
-                    Allow Surrender:
-                </div>
-                <div className="left-toggles">
-                    <Switch id="darkMode" checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
-                    <Switch id="soft17" checked={soft17} onChange={() => setSoft17(!soft17)} />
-                    <Switch id="surrender" checked={surrender} onChange={() => setSurrender(!surrender)} />
-                </div>
-                <div className="percentages">
-                    Session Percentage Correct:
-                    <br></br>
-                    Overall Percentage Correct:
-                </div>
-                <div className="right-options">
-                    Hand Timer:
-                </div>
-                <div className="right-toggles">
-                    <Switch id="handTimer" checked={handTimer} onChange={() => setHandTimer(!handTimer)} />
-                </div>
-                <div className="streaks">
-                    Best Session Streak:
-                    <br></br>
-                    Longest Streak:
-                </div>
-            </div>
+      <div className={`acct-container ${darkMode ? 'dark-mode' : ''}`}>
+        <div className="left-options">
+          Dark Mode:
+          <br />
+          Hit on Soft 17:
+          <br />
+          Allow Surrender:
         </div>
-    );
+        <div className="left-toggles">
+          <Switch id="darkMode" checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+          <Switch id="soft17" checked={soft17} onChange={() => setSoft17(!soft17)} />
+          <Switch id="surrender" checked={surrender} onChange={() => setSurrender(!surrender)} />
+        </div>
+        <div className="percentages">
+          Session Percentage Correct:
+          <br />
+          Overall Percentage Correct:
+        </div>
+        <div className="right-options">
+          Hand Timer:
+        </div>
+        <div className="right-toggles">
+          <Switch id="handTimer" checked={handTimer} onChange={() => setHandTimer(!handTimer)} />
+          {handTimer && (
+            <div>
+              Timer Duration:
+              <select value={timerDuration} onChange={(e) => handleTimerChange(parseInt(e.target.value))}>
+                <option value={5}>5 seconds</option>
+                <option value={10}>10 seconds</option>
+                <option value={30}>30 seconds</option>
+              </select>
+            </div>
+          )}
+        </div>
+        <div className="streaks">
+          Best Session Streak:
+          <br />
+          Longest Streak:
+        </div>
+      </div>
+    </div>
+  );
 }
-
 
 export default Account;
--- React/src/Play.js
+++ React/src/Play.js
@@ -1,203 +1,201 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from './Auth';
-import { useTimer } from './TimerContext';
 import './Play.css';
-
-
-// TODO:
-// Nav bar
-// Implement split and (double down, maybe?) functionality
-// Real time feedback on plays, can take this from Algorith.js
-// Stylization changes
-// Port tracking to account database, when me make that.
-// Card variations other than clubs
-// Hole card differentation
-// Surrender functionality
-
-// Need to commment everything all functions at least
 
 const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
-function Play() {
+function Play({timerDuration}) {
+  const { isAuthenticated, logout } = useAuth();
+  const { userDetails } = useAuth();
 
+  const [playerHand, setPlayerHand] = useState([]);
+  const [dealerHand, setDealerHand] = useState([]);
+  const [gameState, setGameStatus] = useState("Deal Cards to Start");
+  const [timer, setTimer] = useState(10); // Initial timer value in seconds
+  const [isTimeUp, setIsTimeUp] = useState(false);
 
-    const { isAuthenticated, logout } = useAuth();
-    const { userDetails } = useAuth();
-    const { timerEnabled: contextTimerEnabled } = useTimer();
+  useEffect(() => {
+    let countdown;
 
-    const [playerHand, setPlayerHand] = useState([]);
-    const [dealerHand, setDealerHand] = useState([]);
-    const [gameState, setGameStatus] = useState("Deal Cards to Start");
-    const [timeUp, setTimeUp] = useState(false);
-    const { timerEnabled, timerDuration } = useTimer();
-
-    useEffect(() => {
-        let timer;
-      
-        const handleTimeout = () => {
-          setTimeUp(true);
-        };
-      
-        if (timerEnabled && contextTimerEnabled) {
-          timer = setTimeout(handleTimeout, timerDuration * 1000);
-        }
-      
-        return () => {
-          clearTimeout(timer);
-        };
-      }, [timerEnabled, timerDuration, contextTimerEnabled]);
-
-    const calculateHandValue = (hand) => {
-        let value = 0;
-        let aces = 0;
-        for (let card of hand) {
-            if (['J', 'Q', 'K'].includes(card)) {
-                value += 10;
-            } else if (card === 'A') {
-                value += 11;
-                aces += 1;
-            } else {
-                value += parseInt(card);
-            }
-        }
-
-        while (value > 21 && aces) {
-            value -= 10;
-            aces -= 1;
-        }
-
-        return value;
+    const handleTimeout = () => {
+      setTimer(timerDuration); // Reset the timer
+      setIsTimeUp(true);
     };
 
-    const dealCards = () => {
-        const playerCards = [cardValues[Math.floor(Math.random() * 13)], cardValues[Math.floor(Math.random() * 13)]];
-        const dealerCards = [cardValues[Math.floor(Math.random() * 13)]];
-        setPlayerHand(playerCards);
-        setDealerHand(dealerCards);
-        setGameStatus("Player's Turn");
-        setTimeUp(false);
+    if (timer > 0) {
+      countdown = setTimeout(() => setTimer(timer - 1), 1000); // Decrease timer every second
+    } else {
+      handleTimeout();
+    }
+
+    return () => {
+      clearTimeout(countdown);
     };
+  }, [timer, timerDuration]);
 
-    const playerHit = () => {
-        const newCard = cardValues[Math.floor(Math.random() * 13)];
-        const newHand = [...playerHand, newCard];
-        setPlayerHand(newHand);
+  const calculateHandValue = (hand) => {
+    let value = 0;
+    let aces = 0;
+    for (let card of hand) {
+      if (['J', 'Q', 'K'].includes(card)) {
+        value += 10;
+      } else if (card === 'A') {
+        value += 11;
+        aces += 1;
+      } else {
+        value += parseInt(card);
+      }
+    }
 
-        if (calculateHandValue(newHand) > 21) {
-            setGameStatus("Player Busted! Dealer Wins!");
-        }
-    };
+    while (value > 21 && aces) {
+      value -= 10;
+      aces -= 1;
+    }
 
-    const playerStand = () => {
-        setGameStatus("Dealer's Turn");
-        dealerTurn();
-    };
+    return value;
+  };
 
-    const dealerTurn = () => {
+  const dealCards = () => {
+    setIsTimeUp(false); // Reset the time-up state
+    setTimer(10); // Reset the timer
+    const playerCards = [cardValues[Math.floor(Math.random() * 13)], cardValues[Math.floor(Math.random() * 13)]];
+    const dealerCards = [cardValues[Math.floor(Math.random() * 13)]];
+    setPlayerHand(playerCards);
+    setDealerHand(dealerCards);
+    setGameStatus("Player's Turn");
+  };
 
-        // Dont change this, we need to use a push() instead of a spreader function here.
-        // For some reason I was getting an infinte loop where the dealer would have to just
-        // keep drawing cards if the number of cards exceeded two and it would never stop
-        // drawing. 
-        let currentDealerHand = [...dealerHand];
-        let dealerPoints = calculateHandValue(currentDealerHand);
+  const playerHit = () => {
+    if (!isTimeUp) {
+      const newCard = cardValues[Math.floor(Math.random() * 13)];
+      const newHand = [...playerHand, newCard];
+      setPlayerHand(newHand);
 
-        while (dealerPoints < 17) {
-            const newCard = cardValues[Math.floor(Math.random() * 13)];
-            currentDealerHand.push(newCard);
-            dealerPoints = calculateHandValue(currentDealerHand);
-        }
-        
-        // Updates the dealer hand on screen.
-        setDealerHand(currentDealerHand);
-    
-if (dealerPoints > 21) {
-            setGameStatus("Dealer Busted! Player Wins!");
-        } else if (dealerPoints === calculateHandValue(playerHand)) {
-            setGameStatus("Push!");
-        } else if (dealerPoints >= calculateHandValue(playerHand)) {
-            setGameStatus("Dealer Wins!");
-        }
-        
-        else {
-            setGameStatus("Player Wins!");
-        }
-    };
-    
-    const resetGame = () => {
-        setPlayerHand([]);
-        setDealerHand([]);
-        setGameStatus("Deal Cards to Start");
-        setTimeUp(false);
-    };
-    return (
-        <div>
-        <nav>
-            <ul>
-                <li><Link to="/calculator">Play Calculator</Link></li>
-                <li><Link to="/play">Simulator</Link></li>
-                <li><Link to="/rules">Rules</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                {isAuthenticated ? (
-                    <><li><Link to="/account">Account</Link></li>
-                        <li onClick={logout}>Logout</li></>)
-                    :
-                    (<><li><Link to="/login">Login</Link></li>
-                        <li><Link to="/register">Register</Link></li></>)
-                }
-            </ul>
-        </nav>
-    
-        <div className="Play">
-            <h1>Blackjack Simulator</h1>
-            <h2>{gameState}</h2>
-    
-            <div className="play-card-sections">
-                <section className="play-dealer-card-section">
-                    <h2>Dealer's Hand</h2>
-                    {dealerHand.map((card, index) => (
-                        <img src={`/resources/${card}.png`} alt={`${card} card`} className="play-card-image" key={index} />
-                    ))}
-                </section>
-                <center>Hand Value: {calculateHandValue(dealerHand)}</center>
-                <div className="play-middle-display">
-                    {gameState === "Deal Cards to Start" ? (
-                        <button className= "play-button" onClick={dealCards}>Deal Cards</button>
-                    ) : gameState === "Player's Turn" ? (
-                        <>
-                            <button className='play-button' onClick={playerHit}>Hit</button>
-                            <button className='play-button' onClick={playerStand}>Stand</button>
-                            {timerEnabled && !timeUp && (
+      if (calculateHandValue(newHand) > 21) {
+        setGameStatus("Player Busted! Dealer Wins!");
+      }
+      setTimer(10); // Reset the timer
+    }
+  };
+
+  const playerStand = () => {
+    if (!isTimeUp) {
+      setGameStatus("Dealer's Turn");
+      dealerTurn();
+    }
+  };
+
+  const dealerTurn = () => {
+    let currentDealerHand = [...dealerHand];
+    let dealerPoints = calculateHandValue(currentDealerHand);
+
+    while (dealerPoints < 17) {
+      const newCard = cardValues[Math.floor(Math.random() * 13)];
+      currentDealerHand.push(newCard);
+      dealerPoints = calculateHandValue(currentDealerHand);
+    }
+
+    setDealerHand(currentDealerHand);
+
+    if (dealerPoints > 21) {
+      setGameStatus("Dealer Busted! Player Wins!");
+    } else if (dealerPoints === calculateHandValue(playerHand)) {
+      setGameStatus("Push!");
+    } else if (dealerPoints >= calculateHandValue(playerHand)) {
+      setGameStatus("Dealer Wins!");
+    } else {
+      setGameStatus("Player Wins!");
+    }
+  };
+
+  const resetGame = () => {
+    setPlayerHand([]);
+    setDealerHand([]);
+    setGameStatus("Deal Cards to Start");
+    setTimer(10); // Reset the timer
+    setIsTimeUp(false); // Reset the time-up state
+  };
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/calculator">Play Calculator</Link></li>
+          <li><Link to="/play">Simulator</Link></li>
+          <li><Link to="/rules">Rules</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          {isAuthenticated ? (
+            <>
+              <li><Link to="/account">Account</Link></li>
+              <li onClick={logout}>Logout</li>
+            </>
+          ) : (
+            <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            </>
+          )}
+        </ul>
+      </nav>
+
+      <div className="Play">
+        <h1>Blackjack Simulator</h1>
+        <h2>{gameState}</h2>
+
+        <div className="play-card-sections">
+          <section className="play-dealer-card-section">
+            <h2>Dealer's Hand</h2>
+            {dealerHand.map((card, index) => (
+              <img src={`/resources/${card}.png`} alt={`${card} card`} className="play-card-image" key={index} />
+            ))}
+          </section>
+          <center>Hand Value: {calculateHandValue(dealerHand)}</center>
+          <div className="play-middle-display">
+            {gameState === "Deal Cards to Start" ? (
+              <button className="play-button" onClick={dealCards}>
+                Deal Cards
+              </button>
+            ) : gameState === "Player's Turn" ? (
+              <>
+                <button className="play-button" onClick={playerHit} disabled={isTimeUp}>
+                  Hit
+                </button>
+                <button className="play-button" onClick={playerStand} disabled={isTimeUp}>
+                  Stand
+                </button>
+                {!isTimeUp && (
                   <div>
-                    Timer: {timerDuration} seconds remaining
+                    Timer: {timer} seconds remaining
                   </div>
                 )}
-                {timeUp && (
-                  <div>
-                    Time's Up! Click to deal again.
+                {isTimeUp && (
+                  <div className="time-up-message">
+                    TIME'S UP!
                     <button className="play-reset-button" onClick={resetGame}>
-                      Deal Again
+                      Redeal
                     </button>
                   </div>
                 )}
-                        </>
-                    ) : (
-                        <button className='play-reset-button' onClick={resetGame}>Reset Game</button>
-                    )}
-                </div>
+              </>
+            ) : (
+              <button className="play-reset-button" onClick={resetGame}>
+                Reset Game
+              </button>
+            )}
+          </div>
 
-                <section className="play-player-card-section">
-                    <h2>Player Hand</h2>
-                    {playerHand.map((card, index) => (
-                        <img src={`/resources/${card}.png`} className="play-card-image" key={index} />
-                    ))}
-                </section>
-                <center>Hand Value: {calculateHandValue(playerHand)}</center>
-            </div>
+          <section className="play-player-card-section">
+            <h2>Player Hand</h2>
+            {playerHand.map((card, index) => (
+              <img src={`/resources/${card}.png`} className="play-card-image" key={index} />
+            ))}
+          </section>
+          <center>Hand Value: {calculateHandValue(playerHand)}</center>
         </div>
-        </div>
-    );
-                    }
+      </div>
+    </div>
+  );
+}
 
 export default Play;
