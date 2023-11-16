4ed4f1b649245273e38fc72471b8c914a57e7035
mariamconde <condemriam@gmail.com>
2023-11-16 14:07:10 -0500

    dark mode local storage implementation

--- React/src/Settings.js
+++ React/src/Settings.js
@@ -1,17 +1,23 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const SettingsContext = createContext();
 
 export const useSettings = () => useContext(SettingsContext);
 
 export const SettingsProvider = ({ children }) => {
-  const [settings, setSettings] = useState({
+  const storedSettings = JSON.parse(localStorage.getItem('settings'));
+  const initialSettings = storedSettings || {
     darkMode: false,
     soft17: false,
     surrender: false,
     handTimer: false,
     timerDuration: 10
-  });
+  };
+
+  const [settings, setSettings] = useState(initialSettings);
+  useEffect(() => {
+    localStorage.setItem('settings', JSON.stringify(settings));
+  }, [settings]);
 
   return (
     <SettingsContext.Provider value={{ settings, setSettings }}>
